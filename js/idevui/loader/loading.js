5e27840f31fc507c79e144d4aca4a44621c50a3f
Trakware Solutions <support@trakware-solutions.co.uk>
2013-07-03 11:24:24 +0100

    Hot Fix 130703.1
    Fixed loader to work in IE8

--- js/idevui/loader/loading.js
+++ js/idevui/loader/loading.js
@@ -12,7 +12,9 @@ var idevLoader = {
         this.width = 300;
         this.html = "<div id='_idevloader' class='ui-loader-progress ui-modern ui-ie9' style='position:absolute;left:0px;width:0px;'>";
         this.html += "</div>";
+        if(navigator.userAgent.toLowerCase().indexOf('msie 8') != -1) this.html += "<span style='position:absolute;left:0px;width:"+this.width+"px;text-align:center;'>Loading...</span>";
         this.div = null;
+        this.first = true;
     },
     render:function()
     {
@@ -27,14 +29,19 @@ var idevLoader = {
                 })
                 return;
             }
-            var oDiv=document.createElement("DIV");
-            oDiv.id = "loader";
-            oDiv.className = "ui-loader ui-modern ui-ie9";
-            oDiv.innerHTML = this.html;
-            this.div = oDiv;
-            document.body.appendChild(oDiv);
 
-            if(typeof jQuery == 'undefined')
+            if(this.first)
+            {
+                var oDiv=document.createElement("DIV");
+                oDiv.id = "loader";
+                oDiv.className = "ui-loader ui-modern ui-ie9";
+                oDiv.innerHTML = this.html;
+                this.div = oDiv;
+                document.body.appendChild(oDiv);
+                this.first = false;
+            }
+
+            if(typeof jQuery == 'undefined' && !idev.isIE8())
             {
                 idev.internal.addScript(_preferences.libpath+"jquery191min.js");
                 $delay(100,function()
@@ -44,41 +51,44 @@ var idevLoader = {
                 return;
             }
 
-            if(_preferences.splashScreen == "" || !_preferences.splashScreen)
+            if(!idev.isIE8())
             {
-                idev.internal.addStyle(_preferences.libpath+"loader/jquery.percentageloader.css");
-                
-                if(typeof jQuery != 'undefined')
+                if(_preferences.splashScreen == "" || !_preferences.splashScreen)
                 {
-                    this.loader = null;
-                    this.progress = 0;
-                    var that = this;
-                    idev.internal.addScript(_preferences.libpath+"loader/jquery.percentageloader.min.js",function(){
-                        var loader = $('#_idevloader').percentageLoader({
-                            width: 200,
-                            height: 200,
-                            progress: 0,
-                            value: 'Loading'
+                    idev.internal.addStyle(_preferences.libpath+"loader/jquery.percentageloader.css");
+                    
+                    if(typeof jQuery != 'undefined')
+                    {
+                        this.loader = null;
+                        this.progress = 0;
+                        var that = this;
+                        idev.internal.addScript(_preferences.libpath+"loader/jquery.percentageloader.min.js",function(){
+                            var loader = $('#_idevloader').percentageLoader({
+                                width: 200,
+                                height: 200,
+                                progress: 0,
+                                value: 'Loading'
+                            });
+                            that.loader = loader;
                         });
-                        that.loader = loader;
-                    });
+                    }
+                    else
+                    {
+                        idev.internal.addScript(_preferences.libpath+"jquery191min.js");
+                        $delay(100,function()
+                        {
+                            idevLoader.render();
+                        });
+                        return;
+                    }
                 }
                 else
                 {
-                    idev.internal.addScript(_preferences.libpath+"jquery191min.js");
-                    $delay(100,function()
-                    {
-                        idevLoader.render();
-                    });
-                    return;
-                }
-            }
-            else
-            {
-                var loader = $('#loader');
-                loader.append("<image src='"+_preferences.imagepath+_preferences.splashScreen+"' />");
+                    var loader = $('#loader');
+                    loader.append("<image src='"+_preferences.imagepath+_preferences.splashScreen+"' />");
 
-                this.loader = loader;
+                    this.loader = loader;
+                }
             }
 
             $delay(200,function()
@@ -98,31 +108,44 @@ var idevLoader = {
             if (this.div == null) return;
             if (nStep > this.steps) return;
             
-            if(_preferences.splashScreen == "" || !_preferences.splashScreen)
+            if(!idev.isIE8())
             {
-                var addPercent = 1 / idev.dependancy.length;
-                var loopCount = 0;
-                var roundedValue = Math.round(addPercent * 100);
-                var that = this;
-                var loop = window.setInterval(function(){
-                    if(loopCount == roundedValue)
-                    {
-                        // Last Loop
-                        that.progress = that.progress + (addPercent - (roundedValue / 100));
-                    }
+                if(_preferences.splashScreen == "" || !_preferences.splashScreen)
+                {
+                    var addPercent = 1 / idev.dependancy.length;
+                    var loopCount = 0;
+                    var roundedValue = Math.round(addPercent * 100);
+                    var that = this;
+                    var loop = window.setInterval(function(){
+                        if(loopCount == roundedValue)
+                        {
+                            // Last Loop
+                            that.progress = that.progress + (addPercent - (roundedValue / 100));
+                        }
 
-                    if(loopCount >= roundedValue)
-                    {
-                        loopCount = 0;
-                        window.clearInterval(loop);
-                    }
-                    else
-                    {
-                        that.progress = that.progress + 0.01;
-                        that.loader.setProgress(that.progress);
-                    }
-                    loopCount++;
-                },25);
+                        if(loopCount >= roundedValue)
+                        {
+                            loopCount = 0;
+                            window.clearInterval(loop);
+                        }
+                        else
+                        {
+                            that.progress = that.progress + 0.01;
+                            that.loader.setProgress(that.progress);
+                        }
+                        loopCount++;
+                    },25);
+                }
+            }
+            else
+            {
+                if(_preferences.splashScreen == "" || !_preferences.splashScreen)
+                {
+                    var prg = document.getElementById("_idevloader");
+                    var w = prg.style.width;
+                    w = parseInt(this.width / this.steps * nStep);
+                    prg.style.width = w + "px";
+                }
             }
         }
         catch(e)
@@ -134,8 +157,15 @@ var idevLoader = {
         if (this.div == null) return;
         $delay(150,function(wgt)
         {
-            var div = $('#_idevloader');
-            div.remove();
+            if(!idev.isIE8())
+            {
+                var div = $('#_idevloader');
+                div.remove();
+            }
+            else
+            {
+                document.body.removeChild(wgt.div);
+            }
             wgt.div = null;
         },this);
         if(_preferences.splashScreen != "" && _preferences.splashScreen)
